6447c7db8d1874926a246bf1b086bacfb74d9566
Pedro <pedrowilker1516@gmail.com>
2022-09-04 23:57:47 -0300

    Add mario in game and fixing keydown errors

--- js/script.js
+++ js/script.js
@@ -1,10 +1,36 @@
-
-const mario = document.querySelector(".mario");
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
 
 const jump = () => {
 
-    mario.classList.add(" jump");
+    document.querySelector('.mario').classList.add("jump");
+
+    setTimeout(() => {
+        document.querySelector('.mario').classList.remove("jump");
+    },500)
 
 }
 
-document.addEventListener("keydown",jump);
+const loop = setInterval(()=>{
+    
+    const pipePosition = document.querySelector('.pipe').offsetLeft;
+    const marioPosition = +window.getComputedStyle(document.querySelector('.mario')).bottom.replace('px','');
+    console.log(marioPosition);
+
+    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
+
+        document.querySelector('.pipe').style.animation="none";
+        document.querySelector('.pipe').style.left=`${pipePosition}px`;
+
+        document.querySelector('.mario').style.animation="none";
+        document.querySelector('.pipe').bottom=`${marioPosition}px`;
+
+        document.querySelector('.mario').src= './images/game-over.png';
+        document.querySelector('.mario').style.width="75px";
+        document.querySelector('.mario').style.marginLeft="50px";
+
+        clearInterval(loop);
+    }
+},10)
+
+document.addEventListener('keydown',jump);
